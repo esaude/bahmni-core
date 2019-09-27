a14c16efe227a4a6498a03de170e5e24488d2065
= <ekirapapaul@gmail.com>
2019-09-27 15:53:16 +0300

    MPOC-635 optimize query on registration search

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/contract/patient/search/PatientSearchBuilder.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/contract/patient/search/PatientSearchBuilder.java
@@ -28,8 +28,8 @@ public class PatientSearchBuilder {
 			"p.gender as gender, " +
 			"p.birthdate as birthDate, " +
 			"p.death_date as deathDate, " +
-			"final_active_patient.patient_status as patientStatus, " +
-			"final_active_patient.patient_state as patientState, " +
+			"pss2.patient_status as \"patientStatus\",\n" +
+            "pss2.patient_state as \"patientState\"," +
 			"p.birthdate_estimated as birthdateEstimated, " +
 			"p.date_created as dateCreated, " +
 			"v.uuid as activeVisitUuid, " +
@@ -40,16 +40,15 @@ public class PatientSearchBuilder {
 	public static final String FROM_TABLE = " from person p ";
 	public static final String JOIN_CLAUSE = " left join person_name pn on pn.person_id = p.person_id" +
 			" left join person_address pa on p.person_id=pa.person_id and pa.voided = 'false'" +
-			" join (" +
-			" select myquery.patient_id,myquery.patient_status,myquery.patient_state from (" +
-			" select pss.id,pss.patient_id,pss.patient_status, pss.patient_state" +
-			" from patient_status_state pss" +
-			" left join patient_status_state pss2" +
-			" on pss.patient_id=pss2.patient_id and pss.id < pss2.id"+
-			" where pss2.id IS NULL"+
-			" ) myquery" +
-			" ) as final_active_patient" +
-			" on p.person_id=final_active_patient.patient_id" +
+			" join patient_status_state pss on pss.patient_id=p.person_id\n" +
+            "       inner join (" +
+            "           select patient_id," +
+            "                   patient_status," +
+            "                   patient_state," +
+            "                   max(date_created) date_created" +
+            "           from patient_status_state" +
+            "           GROUP BY patient_id" +
+            "       )pss2 ON pss2.patient_id=pss.patient_id AND pss2.date_created=pss.date_created"+
 			" JOIN (SELECT identifier, patient_id" +
 			"      FROM patient_identifier pi" +
 			" JOIN patient_identifier_type pit ON pi.identifier_type = pit.patient_identifier_type_id AND pi.voided IS FALSE AND pit.retired IS FALSE" +
